2e8e08f5933fca3fcb7f6db53114911a94494c51
Aditya Ranjan <57028840+adityaranjan182@users.noreply.github.com>
2020-07-20 23:59:15 +0530

    Create searching-1.cpp

---
+++ Searching/searching-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+//iterative method
+int binary_search1(int arr[], int n, int element){
+    int low = 0, high = n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid] == element) return mid;
+        else if(arr[mid]> element) return high = mid - 1;
+        else low = mid + 1;
+        return -1;
+    }
+}
+//recursive methid
+int binary_search2(int arr[], int low, int high, int element){
+    int mid = (low+high)/2;
+    if (low>high) return -1;
+    else if (arr[mid]>element) return binary_search2(arr,low,mid-1,element);
+    else return binary_search2(arr,mid+1,high,element);
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,-10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    std::cout<<binary_search1(arr,n,6)<<"\n";
+    std::cout<<binary_search2(arr,0,n-1,6);
+}
+
