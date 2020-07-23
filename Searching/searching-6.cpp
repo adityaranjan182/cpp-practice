50f8fcde04fe6e0f31d5d05f6454cc50e070f8ef
Aditya Ranjan <57028840+adityaranjan182@users.noreply.github.com>
2020-07-23 10:09:39 +0530

    floor value of square root of a number

---
+++ Searching/searching-6.cpp
@@ -0,0 +1,21 @@
+//floor value of square root of a number
+#include <iostream>
+
+int squareRoot(int x){
+    int low = 1, high = x/2, ans = -1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(mid*mid==x) return mid;
+        else if(mid*mid>x) high = mid-1;
+        else{
+            low = mid+1;
+            ans = mid;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    std::cout<<squareRoot(9)<<"\n";
+    std::cout<<squareRoot(17);
+}
