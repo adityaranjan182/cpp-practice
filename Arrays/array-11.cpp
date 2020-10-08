1933e3c15a3c14ba66c77256e6e1570ef2eaa350
Vasu Sehgal <44269903+The-Geek-Monk@users.noreply.github.com>
2020-10-08 14:37:00 +0530

    Updated Array-11 #HActober2020

--- Arrays/array-11.cpp
+++ Arrays/array-11.cpp
@@ -4,8 +4,8 @@
 
 #include <iostream>
 using namespace std;
-
-int max_diff(int arr[],int n)
+/* Edited by Vasu Sehgal*/
+int max_difference(int arr[],int n)
 {
     int min_val = arr[0]; int diff = arr[1]-arr[0];
     for (int i=1;i<n;i++)
@@ -20,5 +20,5 @@ int main()
 {
     int arr[] = {2, 3, 10, 6, 4, 8, 1};
     int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<max_diff(arr,n);
+    cout<<max_difference(arr,n);
 }
