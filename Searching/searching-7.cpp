233875c769756d01b7434f31b084f3e9abbf120d
Aditya Ranjan <57028840+adityaranjan182@users.noreply.github.com>
2020-10-16 12:40:25 +0530

    unbounded binary search

--- Searching/searching-7.cpp
+++ Searching/searching-7.cpp
@@ -19,7 +19,7 @@ int findIndex(int arr[], int x){
     if (arr[0]==x) return 0;
     int i = 1;
     while(arr[i]<x) i = i*2;
-    if(arr[i]==x) return x;
+    if(arr[i]==x) return i;
     binary_search(arr,i/2+1,i-1,x);
 }
 
